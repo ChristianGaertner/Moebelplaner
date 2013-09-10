71b98982d73d2cda999b341b96142b699384d33c
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-09-10 22:12:34 +0200

    Added build scripts and some basic infrastructure

--- src/io/github/christiangaertner/moebelplaner/Moebelplaner.java
+++ src/io/github/christiangaertner/moebelplaner/Moebelplaner.java
@@ -1,19 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.github.christiangaertner.moebelplaner;
+
+import io.github.christiangaertner.moebelplaner.graphics.Renderer;
 
 /**
  *
  * @author Christian
  */
-public class Moebelplaner {
+public final class Moebelplaner {
 
+    private Renderer renderer;
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
---
+++ src/io/github/christiangaertner/moebelplaner/graphics/Renderer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.christiangaertner.moebelplaner.graphics;
+
+/**
+ *
+ * @author Christian
+ */
+public class Renderer {
+    
+}
