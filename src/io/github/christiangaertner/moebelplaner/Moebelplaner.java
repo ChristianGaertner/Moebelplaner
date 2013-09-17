11f9f2632c3682cf9782f5f80a1619a4c686cc31
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-09-17 22:25:35 +0200

    Switched to debug mode again for developmente

--- src/io/github/christiangaertner/moebelplaner/Moebelplaner.java
+++ src/io/github/christiangaertner/moebelplaner/Moebelplaner.java
@@ -347,7 +347,7 @@ public final class Moebelplaner extends Canvas implements Runnable {
         }
 
         // Unseren Moebelplaner erstellen (im Debug mode)
-        Moebelplaner planer = new Moebelplaner();
+        Moebelplaner planer = new Moebelplaner(true);
 
         // Sonst wird es schwieriger mit den Graphiken
         planer.frame.setResizable(false);
